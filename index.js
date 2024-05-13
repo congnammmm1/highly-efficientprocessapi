7abc17ea188cd36a37075cc42e525eeea07ddf7d
congnammmm1 <dr.elgabrypun0suy22@hotmail.com>
2024-05-13 18:34:18 +0700

    add more features

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function levelOrder(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const size = queue.length;
-    const level = [];
-    for (let i = 0; i < size; i++) {
-      const node = queue.shift();
-      level.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    result.push(level);
-  }
-  return result;
-}
+const firstName = "John";
+const lastName = "Doe";
+const fullName = firstName + " " + lastName;
+console.log(fullName); // John Doe
